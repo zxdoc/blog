db55e8cde8a89e9cd1da500696f370febb3df0f1
赵x <x@qq.com>
2024-05-08 15:34:23 +0800

    测试自动部署

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,176 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  // base: "/blog/",
+  head:[
+      ['link',{ rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'referrer', content: 'no-referrer' }]
+  ],
+  title: "老赵",
+  description: "",
+  themeConfig: {
+    docFooter: {
+      prev: "上一篇",
+      next: "下一篇",
+    },
+    lastUpdated: {
+      text: "最后更新时间",
+      // formatOptions: {
+      //   dateStyle: "full",
+      //   timeStyle: "medium",
+      // },
+    },
+    logo: "/像素_猫.svg",
+    // https://vitepress.dev/reference/default-theme-config
+    outline: {
+      label: "本页目录",
+    },
+    search: {
+      provider: "local",
+    },
+    nav: [
+      {
+        text: "我的日常",
+        items: [
+          {
+            text: "windows系统配置别名",
+            link: "/我的日常/windows配置命令别名",
+          },
+          {
+            text: "软件卸载错误",
+            link: "/我的日常/安装或者卸载软件时遇到trying to use is on a network resource that is unavailable",
+          },{
+            text: "美化windows终端",
+            link: "/我的日常/美化windows终端",
+          },
+        ],
+      },{
+        text: "前端",
+        items: [
+          {
+            text: "网格布局",
+            link: "/前端/网格布局/1.容器声明",
+          },
+          {
+            text: "Electron",
+            link: "/前端/electron/1.Electron由来",
+          },
+          {
+            text: "Flex布局",
+            link: "/前端/flex布局/容器属性/1.弹性盒",
+          }
+        ],
+      },{
+        text: "杂七杂八",
+        items: [
+          {
+            text: "考勤",
+            link: "/杂七杂八/考勤",
+          }
+        ],
+      },
+    ],
+    sidebar: {
+      "/我的日常": [
+        {
+          text: "windows系统配置别名",
+          link: "/我的日常/windows配置命令别名",
+        },
+        {
+          text: "软件卸载错误",
+          link: "/我的日常/安装或者卸载软件时遇到trying to use is on a network resource that is unavailable",
+        },{
+          text: "美化windows终端",
+          link: "/我的日常/美化windows终端",
+        },
+      ],
+      "/前端/网格布局": [
+        {
+          text: "开始",
+          items: [{
+            text: "容器声明",
+            link: "/前端/网格布局/1.容器声明",
+          },{
+            text: "划分行列",
+            link: "/前端/网格布局/2.划分行列",
+          },{
+            text: "设置间距",
+            link: "/前端/网格布局/3.设置间距",
+          },{
+            text: "栅格线部署",
+            link: "/前端/网格布局/4.栅格线部署",
+          },{
+            text: "区域部署",
+            link: "/前端/网格布局/5.区域部署",
+          },{
+            text: "栅格控制",
+            link: "/前端/网格布局/6.栅格控制",
+          },{
+            text: "栅格简写",
+            link: "/前端/网格布局/7.栅格简写",
+          }]
+        }
+      ],
+      "/前端/electron": [
+        {
+          text: "开始",
+          items: [{
+            text: "Electron由来",
+            link: "/前端/electron/1.Electron由来",
+          },{
+            text: "Electron架构",
+            link: "/前端/electron/2.Electron架构",
+          },]
+        }
+      ],
+      "/前端/flex布局":[
+        {
+          text: "容器属性",
+          items: [{
+            text: "弹性盒子",
+            link: "/前端/flex布局/容器属性/1.弹性盒",
+          },{
+            text: "主轴方向",
+            link: "/前端/flex布局/容器属性/2.主轴方向",
+          },{
+            text: "换行",
+            link: "/前端/flex布局/容器属性/3.换行",
+          },{
+            text: "主轴对齐方式",
+            link: "/前端/flex布局/容器属性/4.主轴对齐方式",
+          },{
+            text: "交叉轴对齐方式",
+            link: "/前端/flex布局/容器属性/5.交叉轴对齐方式",
+          },{
+            text: "多交叉轴对齐方式",
+            link: "/前端/flex布局/容器属性/6.多交叉轴对齐方式",
+          }]
+        },
+        {
+          text: "项目属性",
+          items: [{
+            text: "排序",
+            link: "/前端/flex布局/项目属性/1.排序",
+          },{
+            text: "flex-basis",
+            link: "/前端/flex布局/项目属性/2.flex-basis.md",
+          },{
+            text: "放大比例",
+            link: "/前端/flex布局/项目属性/3.放大比例.md",
+          },{
+            text: "缩小比例",
+            link: "/前端/flex布局/项目属性/4.缩小比例.md",
+          },{
+            text: "独立对齐方式",
+            link: "/前端/flex布局/项目属性/5.独立对齐方式",
+          }]
+        }
+      ]
+    },
+
+    socialLinks: [
+      // { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,25 @@
+---
+layout: home
+
+hero:
+  image: /logo.png
+  name: "学习笔记"
+  text: "当猫咪悄悄从门缝钻进来，你就知道它们正在密谋着什么大事情了！"
+  tagline: 不积跬步，无以至千里
+  actions:
+    - theme: brand
+      text: 最新文章
+      link: /前端/flex布局/项目属性/5.独立对齐方式
+
+features:
+  - icon: "😺"
+    title: "与猫为伴，心灵的港湾"
+    details: "享受与毛茸茸小伙伴相伴的温馨时光，尽情沉浸在爱与快乐中。"
+  - icon: "🐾"
+    title: "探索猫咪的世界"
+    details: "跟随猫咪的脚步，发现隐藏在他们眼中的秘密，领略生活的美好。"
+  - icon: "🐱"
+    title: "猫咪的奇妙冒险"
+    details: "与可爱的猫咪一起展开冒险之旅，畅游在无尽的惊喜与欢乐之中。"
+
+---
